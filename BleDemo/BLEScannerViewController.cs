3e83b4537b8ef596942fdb7c22f3f7b3afc18115
anand madhav <anand.madhav@lntinfotech.com>
2020-07-26 23:04:43 +0530

    * Info.plist:
    * BLEScannerViewController.cs: added import file code

--- BleDemo/BLEScannerViewController.cs
+++ BleDemo/BLEScannerViewController.cs
@@ -7,6 +7,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using UIKit;
+using MobileCoreServices;
+using Foundation;
 
 namespace BleDemo
 {
@@ -15,6 +17,15 @@ namespace BleDemo
         private IAdapter adapter;
         private IBluetoothLE bluetoothLE;
         private IList<IDevice> deviceList = new List<IDevice>();
+        private string[] allowedUTIs =  {
+                    UTType.UTF8PlainText,
+                    UTType.PlainText,
+                    UTType.RTF,
+                    UTType.PNG,
+                    UTType.Text,
+                    UTType.PDF,
+                    UTType.ZipArchive
+                };
 
         public BLEScannerViewController(IntPtr handle) : base(handle)
         {
@@ -44,6 +55,11 @@ namespace BleDemo
             adapter.DeviceDisconnected += OnDeviceDisConnected;
 
             StartScanningBle();
+        }
+
+        private void Picker_WasCancelled(object sender, EventArgs e)
+        {
+            Console.WriteLine("Picker was Cancelled");
         }
 
         public override void ViewDidDisappear(bool animated)
@@ -76,6 +92,41 @@ namespace BleDemo
             var characterstic = characteristics.FirstOrDefault();
             var bytes = await characterstic.ReadAsync();
             uitableView.ReloadData();
+            var picker = new UIDocumentPickerViewController(allowedUTIs, UIDocumentPickerMode.Open);
+            picker.WasCancelled += Picker_WasCancelled;
+          
+            picker.DidPickDocumentAtUrls += (object s, UIDocumentPickedAtUrlsEventArgs e) =>
+            {
+                Console.WriteLine("url = {0}", e.Urls[0].AbsoluteString);
+                //bool success = await MoveFileToApp(didPickDocArgs.Url);  
+                var success = true;
+                string filename = e.Urls[0].LastPathComponent;
+                string msg = success ? string.Format("Successfully imported file '{0}'", filename) : string.Format("Failed to import file '{0}'", filename);
+                // Some invaild file url returns null  
+                NSData data = NSData.FromUrl(e.Urls[0]);
+                if (data != null)
+                {
+                    byte[] dataBytes = new byte[data.Length];
+
+                    System.Runtime.InteropServices.Marshal.Copy(data.Bytes, dataBytes, 0, Convert.ToInt32(data.Length));
+
+                    for (int i = 0; i < dataBytes.Length; i++)
+                    {
+                        Console.WriteLine(dataBytes[i]);
+                    }
+                }
+
+                Console.WriteLine(data + "Completed");
+
+                var alertController = UIAlertController.Create("import", msg, UIAlertControllerStyle.Alert);
+                var okButton = UIAlertAction.Create("OK", UIAlertActionStyle.Default, (obj) =>
+                {
+                    alertController.DismissViewController(true, null);
+                });
+                alertController.AddAction(okButton);
+                PresentViewController(alertController, true, null);
+            };
+            PresentViewController(picker, true, null);
         }
 
         private  void OnDeviceDisConnected(object sender, DeviceEventArgs eventArgs)
